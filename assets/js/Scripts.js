62991ecaad84775fda947c78fe099da35a641c81
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-05-26 23:38:26 -0300

    add

--- assets/js/Scripts.js
+++ assets/js/Scripts.js
@@ -1,16 +1,6 @@
 const btnRetirar = document.querySelector('.escolhaRetirada')
 const btnLevar = document.querySelector('.escolhaLevar')
 
-let nomeUsuario = (window.prompt ('Diga um apelido antes de iniciarmos'))
-const nome = document.querySelector('#nome')
-
-if (nomeUsuario === null || nomeUsuario.length <= 0 ) {
-    nome.innerHTML = `Bem vindxs!`
-}
-else if (nomeUsuario.length > 0  ) {
-    nome.innerHTML = `Bem vindxs ${nomeUsuario}!!`
-}
-
 btnRetirar.addEventListener('click' , iniciarPedido)
 btnLevar.addEventListener('click' , iniciarPedido)
 
@@ -24,3 +14,15 @@ function iniciarPedido(){
     secao.classList.remove('mostrar')
 
 }
+
+let nomeUsuario = (window.prompt ('Queremos te oferecer o melhor, bora iniciar seu pedido?' , 'Diga seu nome'))
+const nome = document.querySelector('#nome')
+
+if (nomeUsuario === null || nomeUsuario.length == 0 ) {
+    nome.innerHTML = `Bem vindxs!`
+}
+else if (nomeUsuario.length > 0  ) {
+    nome.innerHTML = `Bem vindxs ${nomeUsuario}!!`
+}
+
+
