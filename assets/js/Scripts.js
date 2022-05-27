1479dcb981002715914bb0cd61cc2527ac6c024a
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-05-27 18:37:40 -0300

    trabalhando com javascript

--- assets/js/Scripts.js
+++ assets/js/Scripts.js
@@ -1,3 +1,4 @@
+/* Iniciando a seção de pedido depois da seção de seleção */
 const btnRetirar = document.querySelector('.escolhaRetirada')
 const btnLevar = document.querySelector('.escolhaLevar')
 
@@ -15,14 +16,139 @@ function iniciarPedido(){
 
 }
 
+/* Capturando o nome e imprimindo na tela */
+
 let nomeUsuario = (window.prompt ('Queremos te oferecer o melhor, bora iniciar seu pedido?' , 'Diga seu nome'))
 const nome = document.querySelector('#nome')
 
-if (nomeUsuario === null || nomeUsuario.length == 0 ) {
+if (nomeUsuario === null || nomeUsuario.length == 0 || nomeUsuario == 'Diga seu nome') {
     nome.innerHTML = `Bem vindxs!`
 }
-else if (nomeUsuario.length > 0  ) {
+else {
     nome.innerHTML = `Bem vindxs ${nomeUsuario}!!`
 }
 
+/* Criando template do card produto*/
 
+const cardsVitrine = document.querySelector('.cardsPedidos')
+
+function criarCard (produto) {
+    
+    let li = document.createElement('li')
+
+
+    li.innerHTML = `
+        <img src="${produto.image}" alt="${produto.nome}">
+                
+        <p>${produto.nome}</p>
+        <p>${produto.preco.toFixed(2)}</p>
+
+        <button type="button" id="${produto.id}">
+            Adicionar
+        </button>
+        `
+
+    cardsVitrine.appendChild(li)
+}
+
+/* Listando os cards de produtos*/
+function listarProdutos (listaProdutos){
+    
+    for (let i = 0 ; i < listaProdutos.length ; i ++) {
+    const produto = listaProdutos[i]
+    criarCard(produto)
+}
+}
+
+listarProdutos(produtos)
+
+/* Adicionando produtos no carrinho de compras*/
+
+let carrinho = []
+
+cardsVitrine.addEventListener('click' , addNoCarrinho)
+
+function addNoCarrinho (event) {
+    //identificando elemento clicado
+   const btn = event.target
+   
+
+   if (btn.tagName == "BUTTON") {
+      const idProduto = btn.id
+
+      //Pesquisando se o produto é existente
+      const produtoFiltrado = produtos.find(
+     (produto) => produto.id == idProduto
+    )
+
+    //Adicionando no array carrinho
+    carrinho.push(produtoFiltrado)
+
+        listarPedidos()
+        atualizarTotal()
+}
+}
+
+const listaPedidos = document.querySelector('.listaProdutos')
+
+
+function montarTemplateCarrinho (pedido) {
+
+    const itemLista = document.createElement('li')
+
+    itemLista.innerHTML = `
+        <div class="infoLadoEsquerdo">
+            <img src="${pedido.image}" alt="${pedido.nome}">
+            <p>${pedido.nome}</p>
+        </div>
+        <div class="infoLadoDireito">
+            <span>R$${pedido.preco.toFixed(2)}</span>
+            <button>
+            <img src="./assets/img/lixo.png" alt="icon lixo">
+            </button>
+        </div>
+    `
+
+    listaPedidos.appendChild(itemLista)
+}
+
+function listarPedidos (){
+    
+    listaPedidos.innerHTML = ''
+    for (let i = 0 ; i < carrinho.length ; i ++) {
+    const pedido = carrinho[i]
+    montarTemplateCarrinho(pedido) 
+}
+}
+
+listaPedidos.addEventListener('click' , removeItem)
+
+function removeItem (event) {
+    const botao = event.target
+
+    if (botao.tagName == "BUTTON") {
+     const item = botao.closest("div li") // retorna o ancestral + próximo, em relação ao atual
+     item.remove()
+     
+    
+     carrinho.splice(item , 1)
+     
+     
+
+    }
+
+
+}
+
+
+function atualizarTotal() {
+    const infoPreco = document.querySelector('.infoPreco')
+    
+    let total = 0
+    for(let i = 0 ; i < carrinho.length ; i ++) {
+        const produto = carrinho[i]
+
+        total += produto.preco
+    }
+    infoPreco.innerText = `Total: R$ ${total.toFixed(2)}`
+}
---
+++ assets/js/produtos.js
@@ -0,0 +1,101 @@
+const produtos = [
+    
+    {
+        id:0,
+        nome: "Combo Whopper",
+        preco: 32.90,
+        image: "./assets/img/combo_whopper.png"
+    },
+    {
+        id:1,
+        nome: "BK Chicken",
+        preco: 16.90,
+        image: "./assets/img/bk_chicken.png"
+    },
+    {
+        id:2,
+        nome: "Sorvete",
+        preco: 9.80,
+        image: "./assets/img/sorvete.png"
+    },
+    {
+        id:3,
+        nome: "Cheddar Duplo",
+        preco: 13.90,
+        image: "./assets/img/cheddar_duplo.png"
+    },
+    {
+        id:4,
+        nome: "Combo Whopper Rodeio",
+        preco: 38.90,
+        image: "./assets/img/whopper_rodeio.png" 
+    }, 
+    {
+        id: 5 ,
+        nome: "Onion Rings" ,
+        preco: 11.90 ,
+        image: "./assets/img/onion_rings.png"
+    } ,
+    {
+        id: 6 ,
+        nome: "BK Mix Ovomaltine" ,
+        preco: 9.25,
+        image: "./assets/img/bk_mix.png"
+    } ,
+    {
+        id: 7 ,
+        nome: "Cheddar" ,
+        preco: 11.90,
+        image: "./assets/img/cheddar.png"
+    } ,
+    {
+        id: 8 ,
+        nome: "Shake Brownie" ,
+        preco: 11.35,
+        image: "./assets/img/shake.png"
+    } ,
+    {
+        id: 9 ,
+        nome: "Stacker Duplo Bacon" ,
+        preco: 25.90,
+        image: "./assets/img/whopper_rodeio.png"
+    } ,
+    {
+        id: 10 ,
+        nome: "Mega Stacker Atômico 4.0" ,
+        preco:28.90,
+        image: "./assets/img/maga-stacker-atomico.png"
+    } ,
+    {
+        id: 11 ,
+        nome: "Big King" ,
+        preco:26.90,
+        image: "./assets/img/big-king.png"
+    } ,
+    {
+        id: 12 ,
+        nome: "Shake de Leite Moça e Paçoca" ,
+        preco:16.90,
+        image: "./assets/img/Shake-Especial.png"
+    } ,
+    {
+        id: 13 ,
+        nome: "Sundae de Morango" ,
+        preco:6.90,
+        image: "./assets/img/Sundae-Morango.png"
+    } ,
+    {
+        id: 14 ,
+        nome: "Balde de Batata" ,
+        preco:18.80,
+        image: "./assets/img/Balde-Batata.png"
+    } ,
+    {
+        id: 15 ,
+        nome: "Batata Furiosa" ,
+        preco:19.60,
+        image: "./assets/img/Batata-Furiosa.png"
+    }
+    
+    
+]
