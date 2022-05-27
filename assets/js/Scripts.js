dba37e0b20c49e2fcc227f804175f48cf510fdaf
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-05-26 22:48:20 -0300

    add recursos JS

--- assets/js/Scripts.js
+++ assets/js/Scripts.js
@@ -1,1 +1,26 @@
+const btnRetirar = document.querySelector('.escolhaRetirada')
+const btnLevar = document.querySelector('.escolhaLevar')
 
+let nomeUsuario = (window.prompt ('Diga um apelido antes de iniciarmos'))
+const nome = document.querySelector('#nome')
+
+if (nomeUsuario === null || nomeUsuario.length <= 0 ) {
+    nome.innerHTML = `Bem vindxs!`
+}
+else if (nomeUsuario.length > 0  ) {
+    nome.innerHTML = `Bem vindxs ${nomeUsuario}!!`
+}
+
+btnRetirar.addEventListener('click' , iniciarPedido)
+btnLevar.addEventListener('click' , iniciarPedido)
+
+function iniciarPedido(){
+    const menu = document.querySelector('main')
+    const rodape = document.querySelector('footer')
+    const secao = document.querySelector('.secao')
+
+    menu.classList.add('mostrar')
+    rodape.classList.add('mostrar')
+    secao.classList.remove('mostrar')
+
+}
